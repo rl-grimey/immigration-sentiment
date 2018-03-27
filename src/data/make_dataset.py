299c5909a9be8692defbfb6d5a52371b138623e9
Larson <rlrson@uiowa.edu>
2018-03-27 13:52:32 -0500

    Add .csv file path function.

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -5,6 +5,34 @@ import logging
 from dotenv import find_dotenv, load_dotenv
 
 
+def get_twitter_files(input_filepath):
+    """ Returns a list of all .csv files in the input directory.
+        :param: input_filepath - File path to directory containing raw twitter scrapes.
+        :returns: List of .csv filepaths for importing.
+    """
+    print ('\tGathering file names...')
+    filtered_files = []
+
+    try:
+        files = os.listdir(working_dir)
+
+        # Filter log files keeping only csv/non-hidden files
+        filtered_files = [f for f in files if \
+            (f.endswith('.csv') and not f.startswith('.'))]
+
+        # Create file paths by combining it with our passed directory
+        filtered_files = [os.path.join(input_filepath, f) for f in filtered_files]
+
+    except Exception as error:
+        print ('\tCould not find directory!')
+        print ('\tError: ', error)
+    
+    finally:
+        return filtered_files
+
+
+
+
 @click.command()
 @click.argument('input_filepath', type=click.Path(exists=True))
 @click.argument('output_filepath', type=click.Path())
