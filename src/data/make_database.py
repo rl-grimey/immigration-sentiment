f8ab1c170491aff7fca9faa799f5a4b26a343df6
Larson <rlrson@geosocialdb.geography.uiowa.edu>
2018-03-29 16:01:00 -0500

    Change tweetID/userID to int, because float64.

--- src/data/make_database.py
+++ src/data/make_database.py
@@ -39,40 +39,34 @@ schemas = (
     """
     CREATE TABLE IF NOT EXISTS "raw_tweets" (
         "id" SERIAL NOT NULL,
-	    "tweetID" BIGINT NOT NULL,
-	    "date" TIMESTAMP,
-	    "message" TEXT,
-	    "username" TEXT,
-        "userID" BIGINT NOT NULL,
-	    "language" VARCHAR(10),
-	    "longitude" FLOAT,
+        "tweetID" INT NOT NULL,
+        "date" TIMESTAMP,
+        "message" TEXT,
+        "username" TEXT,
+        "userID" INT NOT NULL,
+        "language" VARCHAR(10),
+        "longitude" FLOAT,
         "latitude" FLOAT,
         "retweet" TEXT,
-	    CONSTRAINT raw_tweets_pk PRIMARY KEY ("id")
+        CONSTRAINT raw_tweets_pk PRIMARY KEY ("id")
     ) WITH (
         OIDS=FALSE
-    );
-    """,
+    );""",
     # In the filtered table we should set the column values.
     """
     CREATE TABLE IF NOT EXISTS "filter_tweets" (
         "id" SERIAL NOT NULL,
-	    "tweetID" BIGINT NOT NULL,
-	    "date" TIMESTAMP,
-	    "message" TEXT,
-	    "username" TEXT,
-        "userID" BIGINT NOT NULL,
-	    "language" VARCHAR(10),
-	    "longitude" FLOAT,
+        "tweetID" INT NOT NULL,
+        "date" TIMESTAMP,
+        "message" TEXT,
+        "username" TEXT,
+        "userID" INT NOT NULL,
+        "language" VARCHAR(10),
+        "longitude" FLOAT,
         "latitude" FLOAT,
         "retweet" TEXT
-    ) WITH (
-        OIDS=FALSE
-    );
-    """,
-    """
-    ALTER TABLE "filter_tweets" ADD CONSTRAINT "filter_fk0" FOREIGN KEY ("id") REFERENCES "raw_tweets"("id");
-    """
+    ) WITH ( OIDS=FALSE );""",
+    """ALTER TABLE "filter_tweets" ADD CONSTRAINT "filter_fk0" FOREIGN KEY ("id") REFERENCES "raw_tweets"("id");"""
 )
 
 
