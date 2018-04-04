a4305bbf82867ce1f7832aef0dcffc3162540577
Ryan Larson <ryan-p-larson@uiowa.edu>
2018-04-04 11:21:57 -0500

    Add case insensitive SQL filtering.

--- src/data/filter.sql
+++ src/data/filter.sql
@@ -3,8 +3,8 @@ SELECT DISTINCT ON ("tweetID") *
 FROM   raw_tweets 
 WHERE  ( message IS NOT NULL ) 
        AND ( retweet IS NULL ) 
-       AND ( Left(LANGUAGE, 2) LIKE 'en' ) 
-       AND ( latitude IS NOT NULL ) 
-       AND ( DATE >= '2016-12-30 04:00:00' 
-             AND DATE < '2017-02-24 04:00:00' ) 
-       AND ( longitude IS NOT NULL );
+       AND ( LEFT(language, 2) ILIKE 'en' ) 
+       AND ( latitude IS NOT NULL )
+       AND ( longitude IS NOT NULL )
+       AND ( DATE >= '2016-12-30 00:00:00' 
+           AND DATE < '2017-02-25 00:00:00' ) ;
